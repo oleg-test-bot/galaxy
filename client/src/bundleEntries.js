0757eb48dd86fee9cb0737f28c36509080e30d2a
Oleg Zharkov <oleg.zharkov22@gmail.com>
2020-10-02 18:37:32 +0200

    vuefy job information

--- client/src/bundleEntries.js
+++ client/src/bundleEntries.js
@@ -99,6 +99,7 @@ export { mountPageEditor } from "components/PageEditor/mount";
 export { mountPageDisplay } from "components/PageDisplay";
 export { mountDestinationParams } from "components/JobDestinationParams";
 export { mountDatasetInformation } from "components/DatasetInformation";
+export { mountJobInformation } from "components/JobInformation";
 
 // Used in common.mako
 export { default as store } from "storemodern";
